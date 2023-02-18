d111e2b74e006decdfceb9382074805794eeec75
XzoRit <xzorit@hotmail.com>
2023-02-18 17:49:34 +0100

    add test for console functions

--- lib/console/console.test.cpp
+++ lib/console/console.test.cpp
@@ -1,13 +1,32 @@
 #include <console/console.hpp>
+#include <counter/counter.hpp>
 
 #include <boost/test/unit_test.hpp>
 
 namespace
 {
-BOOST_AUTO_TEST_SUITE(test_suite_console);
-BOOST_AUTO_TEST_CASE(test_dummy)
+::counter::model dispatch(::counter::model m, char ev)
 {
-    BOOST_CHECK(1 == 1);
+    return ::counter::update(m, ::console::intent(ev).value());
+}
+BOOST_AUTO_TEST_SUITE(test_suite_console);
+BOOST_AUTO_TEST_CASE(test_increment)
+{
+    ::counter::model m{0};
+    m = dispatch(m, '+');
+    BOOST_TEST(m.value == 1);
+}
+BOOST_AUTO_TEST_CASE(test_decrement)
+{
+    ::counter::model m{2};
+    m = dispatch(m, '-');
+    BOOST_TEST(m.value == 1);
+}
+BOOST_AUTO_TEST_CASE(test_reset)
+{
+    ::counter::model m{2};
+    m = dispatch(m, '.');
+    BOOST_TEST(m.value == 0);
 }
 BOOST_AUTO_TEST_SUITE_END();
 }
