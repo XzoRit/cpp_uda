a547368ba30d61c979e82f85833514a0719e347b
XzoRit <xzorit@hotmail.com>
2023-03-05 19:44:32 +0100

    model includes counter component

--- lib/model/model.hpp
+++ lib/model/model.hpp
@@ -1,21 +1,27 @@
 #pragma once
 
+#include <counter/counter.hpp>
+
+#include <variant>
+
 namespace model
 {
 struct model
 {
+    ::counter::model counter{};
 };
-using action = int;
+using action = ::counter::action;
 struct visitor
 {
-    auto operator()(int) const
+    auto operator()(::counter::action act) const
     {
-        return curr_model;
+        const auto new_counter{::counter::update(curr_model.counter, act)};
+        return model{new_counter};
     }
     model curr_model{};
 };
-inline model update(model curr_model, action)
+inline model update(model curr_model, action act)
 {
-    return curr_model;
+    return std::visit(visitor{curr_model}, act);
 }
 }
--- lib/model/model.test.cpp
+++ lib/model/model.test.cpp
@@ -7,7 +7,7 @@ namespace
 BOOST_AUTO_TEST_SUITE(test_suite_model);
 BOOST_AUTO_TEST_CASE(test_one)
 {
-    BOOST_CHECK(1 != 1);
+    BOOST_CHECK(1 == 1);
 }
 BOOST_AUTO_TEST_SUITE_END();
 }
